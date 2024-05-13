ce905647cc51369489a6d83209fee5b97404cc92
AdelMysterio <storliniadelmo@gmail.com>
2024-05-13 11:54:50 +0200

    corretto esercizio 1

--- esercizio-1/js/script.js
+++ esercizio-1/js/script.js
@@ -1,13 +1,17 @@
 /* L'utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
-const numberA = parseInt(prompt('Inserisci il primo numero'))
-const numberB = parseInt(prompt('inserisci il secondo numero'))
+const numberA = Number.parseInt(prompt('Inserisci il primo numero'), 10)
+const numberB = Number.parseInt(prompt('inserisci il secondo numero'), 10)
 
-if (numberA > numberB) {
-    console.log (numberA)
+if (numberA === numberB) {
+    console.log(numberA, numberB)
+}
+
+else if (numberA > numberB) {
+    console.log(numberA)
 }
 
 else {
-    console.log (numberB)
+    console.log(numberB)
 }
